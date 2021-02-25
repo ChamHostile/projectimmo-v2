d6b594cd5a79c9e3fdc6d40e514d5f011736a2a9
ChamHostile <hamza.aboudou@gmail.com>
2021-02-25 17:01:10 +0100

    feat(annonce): added connection and register on creation feature

--- annonce/urls.py
+++ annonce/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('creer-annonce', views.create_annonce, name='creer-annonce'),
+    path('login-annonce', views.login_user, name='login-annonce'),
+    path('logout-annonce', views.logout_annonce, name='logout-annonce'),
+    path('logged-annonce', views.logged_annonce, name='logged-annonce'),
 
 ]
--- annonce/views.py
+++ annonce/views.py
@@ -5,11 +5,16 @@ from django.contrib.auth.models import Group
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
 
+from .decorators import unauthenticated_user
 from .forms import AnnonceForm, CreateUserForm
 from account.models import Account
 from .models import Annonce
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@unauthenticated_user
 def create_annonce(request):
 
     form = AnnonceForm()
@@ -36,3 +41,44 @@ def create_annonce(request):
     context = {'annonceForm': annonceForm, 'userForm': userForm}
 
     return render(request,'annonce/creer-annonce.html',context)
+
+def login_user(request):
+
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('logged-annonce')
+
+    context = {}
+    return render(request, 'compte/login-annonce.html')
+
+def logout_annonce(request):
+    logout(request)
+    return redirect('creer-annonce')
+
+@login_required(login_url='login-annonce')
+def logged_annonce(request):
+    form = AnnonceForm()
+
+    if request.method == 'POST':
+        annonceForm = AnnonceForm(request.POST)
+
+        if annonceForm.is_valid():
+            annonceForm.save()
+            Annonce.objects.create(
+                user=request.user,
+            )
+            return redirect('/')
+    else:
+        userForm = CreateUserForm()
+        annonceForm = AnnonceForm()
+
+    context = {'annonceForm': annonceForm}
+
+    return render(request, 'annonce/logged-annonce.html', context)
+
